cd9086aeacfe2a6df5138d10441155866131f1c9
Karen Olson <karmarie@gmail.com>
2022-08-26 14:16:36 -0500

    Create HTTPServer

---
+++ src/main/java/echoServer/HTTPServer.java
@@ -0,0 +1,10 @@
+package echoServer;
+
+import java.io.IOException;
+
+public class HTTPServer implements Application {
+    @Override
+    public void call(ReadableWriteable readerWriter) throws IOException {
+        readerWriter.writeLine("HTTP/1.1 200 OK\n\nHello, world");
+    }
+}
---
+++ src/test/java/echoServer/HTTPServerTest.java
@@ -0,0 +1,46 @@
+package echoServer;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HTTPServerTest {
+
+    @Test
+    void itReturnsHelloWorld() throws IOException {
+        TestReaderWriter testReaderWriter = new TestReaderWriter();
+
+        new HTTPServer().call(testReaderWriter);
+
+        assertEquals(List.of("HTTP/1.1 200 OK\n\nHello, world"), testReaderWriter.received());
+    }
+
+
+    private class TestReaderWriter implements ReadableWriteable {
+
+        private final List<String> written = new ArrayList<>();
+
+        @Override
+        public String readLine() {
+            return null;
+        }
+
+        @Override
+        public void writeLine(String message) {
+            written.add(message);
+        }
+
+        public List<String> received() {
+            return written;
+        }
+
+        @Override
+        public void close() throws Exception {
+
+        }
+    }
+}
