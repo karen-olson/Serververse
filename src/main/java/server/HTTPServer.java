02bde0217b5553613cfdaebd6e41d506179c4c35
Karen Olson <karmarie@gmail.com>
2022-09-01 16:28:53 -0500

    Refactor: Rename resources/requestedResource to paths/path

--- src/main/java/server/HTTPServer.java
+++ src/main/java/server/HTTPServer.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HTTPServer implements Application {
-    private final List<String> resources = List.of("/");
+    private final List<String> paths = List.of("/");
 
     @Override
     public void call(ReadableWriteable readerWriter) throws IOException {
         ArrayList<String> request = readRequest(readerWriter);
-        String requestedResource = parseRequest(request);
-        writeResponse(requestedResource, readerWriter);
+        String path = getPath(request);
+        writeResponse(path, readerWriter);
     }
 
     private ArrayList<String> readRequest(ReadableWriteable readerWriter) throws IOException {
@@ -23,13 +23,12 @@ public class HTTPServer implements Application {
         return request;
     }
 
-    private String parseRequest(ArrayList<String> request) {
-        String requestedResource = request.get(0).split("\\s+")[1];
-        return requestedResource;
+    private String getPath(ArrayList<String> request) {
+        return request.get(0).split("\\s+")[1];
     }
 
-    private void writeResponse(String requestedResource, ReadableWriteable readerWriter) throws IOException {
-        if (resources.contains(requestedResource)) {
+    private void writeResponse(String path, ReadableWriteable readerWriter) throws IOException {
+        if (paths.contains(path)) {
             readerWriter.writeLine("HTTP/1.1 200 OK\r\nContent-Length:0\r\n");
         } else {
             readerWriter.writeLine("HTTP/1.1 404 Not Found\r\nContent-Length:0\r\n");
