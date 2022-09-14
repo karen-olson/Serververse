33675cd6ebf5d65f9166c40f3ebc9b37d0f26eb2
Karen Olson <karmarie@gmail.com>
2022-09-14 17:36:43 -0500

    Start extracting Routes table

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -6,19 +6,23 @@ import java.util.Map;
 public class Router implements Routable {
     private final String protocol = "HTTP/1.1";
 
-    Map<String, Response> table = new HashMap<>();
+    Map<String, Response> routes = new HashMap<>();
 
     public Router() {
-        table.put("/", new Response(this.protocol, "200 OK", "Content-Length:0", ""));
-        table.put("/simple_get", new Response(this.protocol, "200 OK", "Content-Length:0", ""));
-        table.put("/simple_get_with_body", new Response(this.protocol, "200 OK", "Content-Length:11", "Hello world"));
-        table.put("/head_request", new Response(this.protocol, "200 OK", "Content-Length:0", ""));
+        populateRoutes();
     }
 
     public Response call(Request request) {
-        Response response = table.get(request.path());
+        Response response = routes.get(request.path());
         Response notFoundResponse = new Response(this.protocol, "404 Not Found", "Content-Length:0", "");
 
         return (response != null) ? response : notFoundResponse;
+    }
+
+    private void populateRoutes() {
+        routes.put("/", new Response(this.protocol, "200 OK", "Content-Length:0", ""));
+        routes.put("/simple_get", new Response(this.protocol, "200 OK", "Content-Length:0", ""));
+        routes.put("/simple_get_with_body", new Response(this.protocol, "200 OK", "Content-Length:11", "Hello world"));
+        routes.put("/head_request", new Response(this.protocol, "200 OK", "Content-Length:0", ""));
     }
 }
